3d4741854148f965e78d79414dc08be25770512b
Isaac Daniel <isaacinfo2017@gmail.com>
2025-09-03 06:59:00 -0300

    feat: listagem de componentes

--- src/app/(dashboard)/componentes/page.tsx
+++ src/app/(dashboard)/componentes/page.tsx
@@ -1,10 +1,34 @@
-export default function Home() {
+'use client';
+
+import { ComponentesTable } from "@/components/organisms/ComponentesTable";
+import { useGetComponentes } from "@/useCases/useGetComponentes";
+import { Plus } from "lucide-react";
+
+export default function Componentes() {
+  const { data: response, isLoading, isError } = useGetComponentes();
+
+  if (isError) {
+    return <div className="text-red-500">Ocorreu um erro ao buscar os componentes.</div>
+  }
+
+  const componentes = response?.data || [];
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-      </footer>
+    <div className="space-y-6">
+      <div className="flex items-center justify-between">
+        <h2 className="text-lg  text-paragraph">
+          Componentes cadastrados
+        </h2>
+        <div className="flex items-center gap-2">
+          <button className="flex items-center gap-2 px-4 py-2 border rounded-md text-sm font-medium bg-primary text-white hover:bg-primary/90">
+            <Plus className="h-4 w-4" />
+            Adicionar
+          </button>
+        </div>
+      </div>
+      <div className="bg-white rounded-lg shadow-sm border border-border p-6 space-y-6">
+        <ComponentesTable data={componentes} isLoading={isLoading} />
+      </div>
     </div>
   );
 }
