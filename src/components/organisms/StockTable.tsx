79e5af428f68e4eddd7e048bf217306d672ab30b
Isaac Daniel <isaacinfo2017@gmail.com>
2025-09-24 22:40:28 -0300

    feat: organismo tabela de estoque

---
+++ src/components/organisms/StockTable.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Stock } from "@/domain/entities/stock";
+import { useReactTable, getCoreRowModel, ColumnDef, flexRender } from "@tanstack/react-table";
+import { ActionsMenu } from "../molecules/ActionsMenu";
+import { LoadingSpinner } from "../molecules/LoadingSpinner";
+
+interface StockTableProps {
+    data: Stock[];
+    isLoading: boolean;
+}
+
+export function StockTable({ data, isLoading }: StockTableProps) {
+    const columns: ColumnDef<Stock>[] = [
+        { accessorKey: "laboratoryName", header: "Laboratório" },
+        { accessorKey: "quantity", header: "Quantidade" },
+        {
+            id: "acoes",
+            header: "Ações",
+            cell: ({ row }) => {
+                return (
+                    <ActionsMenu
+                        onDelete={() => console.log("Deletar estoque")}
+                        onEdit={() => console.log("Editar estoque")}
+                    />
+                )
+            }
+        }
+    ];
+
+    const table = useReactTable({ data, columns, getCoreRowModel: getCoreRowModel() });
+
+    if (isLoading) return <LoadingSpinner text="Buscando estoque..." />;
+    if (data.length === 0) {
+        return <p className="flex flex-col items-center justify-center h-[15vh] gap-4 p-8 text-paragraph">Nenhuma quantidade lançada para este componente</p>;
+    }
+
+    return (
+        <div className="overflow-x-auto">
+            <table className="w-full text-left text-sm">
+                <thead className="border-b border-border text-paragraph font-semibold">
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <th key={header.id} className="py-3 px-4 whitespace-nowrap">
+                                    {flexRender(header.column.columnDef.header, header.getContext())}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody>
+                    {table.getRowModel().rows.map(row => (
+                        <tr key={row.id} className="hover:bg-gray-50">
+                            {row.getVisibleCells().map(cell => (
+                                <td key={cell.id} className="py-3 px-4 text-paragraph whitespace-nowrap">
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
