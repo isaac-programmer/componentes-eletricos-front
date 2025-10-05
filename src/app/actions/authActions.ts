f5f6c1d461083879088d53751700a0e3fbf8e633
Isaac Daniel <isaacinfo2017@gmail.com>
2025-10-05 20:04:36 -0300

    feat(refresh-token): armazenar o refresh token em http only

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -10,17 +10,20 @@ export async function saveTokensToCookies(tokens: { accessToken: string; refresh
     secure: process.env.NODE_ENV === "production",
     sameSite: "strict",
     path: "/",
+    maxAge: 60 * 60 * 24, // 1 dia em segundos
   });
 
-  // cookieStore.set("refresh_token", tokens.refreshToken, {
-  //   httpOnly: true,
-  //   secure: process.env.NODE_ENV === "production",
-  //   sameSite: "strict",
-  //   path: "/",
-  // });
+  cookieStore.set("refresh_token", tokens.refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 7, // 7 dias em segundos
+  });
 }
 
 export async function clearAuthCookies() {
   const cookieStore = await cookies();
   cookieStore.delete("access_token");
+  cookieStore.delete("refresh_token");
 }
