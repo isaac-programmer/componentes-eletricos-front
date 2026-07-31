b4be0a593a0ea5ba614b3fedffbf41a03ee01cb0
Isaac Daniel <isaacinfo2017@gmail.com>
2026-07-31 16:56:52 -0300

    feat: adiciona proxy reverso interno e atualiza instrucoes do docker

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true
-  }
+  },
+  async rewrites() {
+    if (process.env.INTERNAL_API_URL) {
+      return [
+        {
+          source: '/api-interno/:path*',
+          destination: `${process.env.INTERNAL_API_URL}/:path*`, 
+        },
+      ]
+    }
+
+    return []
+  },
 };
 
 export default nextConfig;
